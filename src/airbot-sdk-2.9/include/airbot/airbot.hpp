2717e9c3b427239c975422452e43706548d681b8
IgararA <68152608+IgararA@users.noreply.github.com>
2026-02-24 14:55:40 +0800

    Delete src/airbot-sdk-2.9 directory

--- src/airbot-sdk-2.9/include/airbot/airbot.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef AIRBOT_HPP
-#define AIRBOT_HPP
-#define AIRBOT_VERSION "2.9.0"
-#include "airbot/command/command_base.hpp"
-#include "airbot/utils.hpp"
-#include "airbot/modules/modules.hpp"
-const std::string URDF_INSTALL_PATH = "/usr/share/airbot_models/";
-#endif
